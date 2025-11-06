d38a4c62e8cb189416d20f2fae9f4ce67fe56ccb
Vitor Batalha <vbatalhaferreira@gmail.com>
2025-11-05 22:25:48 -0300

    começando trabalho de cadastro de alunos, pre-funcao

---
+++ cadastro_aluno.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+#define TAMANHO_MAX_NOME 70
+
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct {
+    string nome;
+    Data data_nascimento;
+    string curso;
+    int cpf;
+    string email;
+} Aluno;
+
+Aluno cadastrar_aluno()
+{
+
+}
+
+int main () 
+{
+    cout << "Cadastro de Aluno" << endl;
+
+    while (true)
+    {
+        return 0;
+    }
+}
