70cf269ecd0b0c8c421a9af5d5c6787d2d1e14f8
Vitor Batalha <vbatalhaferreira@gmail.com>
2025-11-05 22:30:51 -0300

    funcao de data de nascimento

--- cadastro_aluno.cpp
+++ cadastro_aluno.cpp
@@ -20,6 +20,38 @@ typedef struct {
     string email;
 } Aluno;
 
+void static limpar_entrada () 
+{
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+Aluno cadastrar_data_nascimento(Aluno a)
+{
+    cout << "Digite a data de nascimento do aluno no formato: (DIA MÊS ANO): " << endl;
+    while (1)
+    {
+        if (cin >> a.data_nascimento.dia >> a.data_nascimento.mes >> a.data_nascimento.ano)
+        {
+            if (a.data_nascimento.dia >= 1 && a.data_nascimento.dia <= 31 &&
+                a.data_nascimento.mes >= 1 && a.data_nascimento.mes <= 12 &&
+                a.data_nascimento.ano > 1900 && a.data_nascimento.ano <= 2025)
+            {
+                return a;
+            }
+            else
+            {
+                cout << "Data inválida. Por favor, digite novamente, no formato: (DIA MÊS ANO): ";
+            }
+        }
+        else
+        {
+            cout << "Entrada inválida. Por favor, digite novamente, no formato: (DIA MÊS ANO): ";
+            limpar_entrada();
+        }
+    }
+}
+
 Aluno cadastrar_aluno()
 {
 
@@ -29,8 +61,8 @@ int main ()
 {
     cout << "Cadastro de Aluno" << endl;
 
-    while (true)
+    while (1)
     {
-        return 0;
+        
     }
 }
