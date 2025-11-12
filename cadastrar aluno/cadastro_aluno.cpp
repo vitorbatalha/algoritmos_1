78988a8384dd8809c375c7ee785e8658f98c0fb0
Vitor Batalha <vbatalhaferreira@gmail.com>
2025-11-12 14:17:06 -0300

    mudanças nas funcoes restantes, função imprimir aluno, melhorias no switch

--- cadastrar aluno/cadastro_aluno.cpp
+++ cadastrar aluno/cadastro_aluno.cpp
@@ -73,55 +73,53 @@ string cadastrar_nome(Aluno a)
 
 string cadastrar_curso(Aluno a)
 {
-    cout << "Digite o curso do aluno: " << endl;
-    if (cin >> a.curso)
+    cout << "Digite o nome do curso do aluno: " << endl;
+    while (1)
     {
-        return a.curso;
+        getline(cin >> ws, a.curso);
+        if (a.curso.length() > 0)
+        {
+            return a.curso;
+        }
+        else
+        {
+            cout << "Entrada inválida. Por favor, digite novamente o nome do curso do aluno: " << endl;
+        }
     }
-    else
-    {
-        cout << "Entrada inválida. Por favor, digite novamente o curso do aluno: " << endl;
-        limpar_entrada();
-    }
-    return a.curso;
 }
 
 string cadastrar_email(Aluno a)
 {
     cout << "Digite o email do aluno: " << endl;
-    if (cin >> a.email)
+    while(1)
     {
-        return a.email;
+        getline(cin >> ws, a.email);
+        if (a.email.size() > 0 && a.email.find('@') != string::npos)
+        {
+            return a.email;
+        }
+        else
+        {
+            cout << "Email inválido. Por favor, digite novamente o email do aluno: " << endl;
+        }
     }
-    else
-    {
-        cout << "Entrada inválida. Por favor, digite novamente o email do aluno: " << endl;
-        limpar_entrada();
-    }
-    return a.email;
 }
 
 string cadastrar_cpf(Aluno a)
 {
     cout << "Digite o CPF do aluno (somente números): " << endl;
-    if (cin >> a.cpf)
+    while (1)
     {
-        if (a.cpf.size() == 11)
+        getline(cin >> ws, a.cpf);
+        if (a.cpf.size() == 11 && a.cpf.find_first_not_of("0123456789") == string::npos)
         {
             return a.cpf;
         }
         else
         {
-            cout << "CPF inválido. Por favor, digite um CPF com 11 dígitos." << endl;
-            limpar_entrada();
+            cout << "CPF inválido. Por favor, digite novamente o CPF do aluno (somente números): " << endl;
         }
     }
-    else
-    {
-        cout << "Entrada inválida. Por favor, digite novamente o CPF do aluno: " << endl;
-        limpar_entrada();
-    }
-    return a.cpf;
 }
 
 void static imprimir_header()
@@ -134,6 +132,19 @@ void static imprimir_header()
     cout << "-----------------------------------------------" << endl;
 }
 
+void static imprimir_aluno(Aluno a)
+{
+    cout << "------------------- Dados do Aluno ------------------" << endl;
+    cout << "Nome: " << a.nome << endl;
+    cout << "Data de Nascimento: " << setw(2) << setfill('0') << a.data_nascimento.dia << "/"
+         << setw(2) << setfill('0') << a.data_nascimento.mes << "/"
+         << a.data_nascimento.ano << endl;
+    cout << "Curso: " << a.curso << endl;
+    cout << "CPF: " << a.cpf << endl;
+    cout << "Email: " << a.email << endl;
+    cout << "-----------------------------------------------------" << endl;
+}
+
 Aluno cadastrar_aluno(Aluno a)
 {
     a.nome = cadastrar_nome(a);
@@ -141,7 +152,46 @@ Aluno cadastrar_aluno(Aluno a)
     a.curso = cadastrar_curso(a);
     a.email = cadastrar_email(a);
     a.cpf = cadastrar_cpf(a);
+
     cout << "Aluno cadastrado com sucesso!" << endl;
+    cout << "Verifique os dados cadastrados abaixo:" << endl;
+
+    imprimir_aluno(a);
+
+    cout << "Deseja alterar algum dado?" << endl;
+    cout << "1. Alterar nome" << endl;
+    cout << "2. Alterar data de nascimento" << endl;
+    cout << "3. Alterar curso" << endl;
+    cout << "4. Alterar email" << endl;
+    cout << "5. Alterar CPF" << endl;
+    cout << "6. Nenhuma alteração, finalizar cadastro" << endl;
+
+    int escolha;
+
+    switch (cin >> escolha, escolha)
+    {
+    case 1:
+        a.nome = cadastrar_nome(a);
+        break;
+    case 2:
+        a.data_nascimento = cadastrar_data_nascimento(a);
+        break;
+    case 3:
+        a.curso = cadastrar_curso(a);
+        break;
+    case 4:
+        a.email = cadastrar_email(a);
+        break;
+    case 5:
+        a.cpf = cadastrar_cpf(a);
+        break;
+    case 6:
+        cout << "Cadastro finalizado." << endl;
+        break;
+    default:
+        cout << "Opção inválida. Finalizando cadastro." << endl;
+        break;
+    }
     return a;
 }
 
@@ -159,10 +209,24 @@ int main()
             {
             case 1:
                 cout << "Opção de cadastrar novo aluno selecionada." << endl;
-                cadastrar_aluno(a);
+                a = cadastrar_aluno(a);
                 break;
             case 2:
                 cout << "Opção de imprimir resultados selecionada." << endl;
+                imprimir_aluno(a);
+                cout << "Deseja continuar cadastrando alunos?" << endl;
+                char continuar;
+                cout << "Digite 's' para sim ou 'n' para não: ";
+                cin >> continuar;
+                if (continuar == 's' || continuar == 'S')
+                {
+                    continue;
+                }
+                else
+                {
+                    cout << "Encerrando o sistema." << endl;
+                    return 0;
+                }
                 break;
             case 3:
                 cout << "Desligando o sistema." << endl;
